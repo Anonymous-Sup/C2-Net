510866b36ea1b15f1770c5b350f0b36958b94d42
GavinYoung1 <162417316+GavinYoung1@users.noreply.github.com>
2024-09-24 13:06:18 +0800

    1way

--- scripts/run_sketchy.sh
+++ scripts/run_sketchy.sh
@@ -10,6 +10,6 @@ cd /home/zhengwei/github/C2-Net/
 CUDA_VISIBLE_DEVICES=1 python train.py --model C2_Net --dataset sketchy \
 --gpu 1 --alpha 0.5 \
 --opt sgd --lr 1e-1 --gamma 1e-1 --epoch 150 --val_epoch 1 --weight_decay 5e-4 --nesterov \
---train_way 5 --test_way 5 --train_shot 1  --test_shot 1 
+--train_way 1 --test_way 1 --train_shot 1  --test_shot 1 
 
 # python3 train.py --model C2_Net --dataset stanford_dog --opt sgd --lr 1e-1 --gamma 1e-1 --epoch 150 --val_epoch 1 --weight_decay 5e-4 --nesterov --train_way 30 --train_shot 5 --train_transform_type 0 --test_shot 1 5 --alpha 0.5 --pre
