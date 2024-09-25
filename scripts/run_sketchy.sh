885ee74dea78ebef084cbacb94aad389d5c75d46
GavinYoung1 <162417316+GavinYoung1@users.noreply.github.com>
2024-09-25 10:02:55 +0800

    update 5w5s

--- scripts/run_sketchy.sh
+++ scripts/run_sketchy.sh
@@ -6,14 +6,14 @@ cd /home/zhengwei/github/C2-Net/
 # --train_way 64 --test_way 64 --train_shot 1  --test_shot 1 \
 # --alpha 0.5 --pre > train_sketchy.log 2>&1 &
 
-# CUDA_VISIBLE_DEVICES=1 nohup python -u train.py --model C2_Net --dataset sketchy \
-# --gpu 1 --alpha 0.5 --detailed_name \
-# --opt adam --lr 1e-3 --gamma 1e-1 --epoch 150 --val_epoch 10 --weight_decay 5e-4 --nesterov \
-# --train_way 5 --test_way 5 --train_shot 1  --test_shot 1 > train_sketchy_conv41e3_adam.log 2>&1 &
+CUDA_VISIBLE_DEVICES=1 nohup python -u train.py --model C2_Net --dataset sketchy \
+--gpu 1 --alpha 0.5 --detailed_name \
+--opt adam --lr 1e-3 --gamma 1e-1 --epoch 150 --val_epoch 10 --weight_decay 5e-4 --nesterov \
+--train_way 5 --test_way 5 --train_shot 5  --test_shot 5 > train_sketchy_conv41e3_adam5s.log 2>&1 &
 
-CUDA_VISIBLE_DEVICES=1 python train.py --model C2_Net --dataset sketchy \
---gpu 1 --alpha 0.5 --resume --detailed_name \
---opt adam --lr 1e-3 --gamma 1e-1 --epoch 150 --val_epoch 1 --weight_decay 5e-4 --nesterov \
---train_way 5 --test_way 5 --train_shot 1  --test_shot 1 
+# CUDA_VISIBLE_DEVICES=1 python train.py --model C2_Net --dataset sketchy \
+# --gpu 1 --alpha 0.5 --resume --detailed_name \
+# --opt adam --lr 1e-3 --gamma 1e-1 --epoch 150 --val_epoch 1 --weight_decay 5e-4 --nesterov \
+# --train_way 5 --test_way 5 --train_shot 1  --test_shot 1 
 
 # python3 train.py --model C2_Net --dataset stanford_dog --opt sgd --lr 1e-1 --gamma 1e-1 --epoch 150 --val_epoch 1 --weight_decay 5e-4 --nesterov --train_way 30 --train_shot 5 --train_transform_type 0 --test_shot 1 5 --alpha 0.5 --pre
